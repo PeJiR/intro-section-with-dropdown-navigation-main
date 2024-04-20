39fd876dc43cb4be2fc298462663e959249b9c37
pejir <pejir84@gmail.com>
2024-04-20 16:01:55 +0200

    update

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const menuBtn = document.getElementById("menu-btn");
 const navbar = document.getElementById("navbar");
 const closeBtn = document.getElementById("close-btn");
+const featuresBtn = document.getElementById("features-btn");
+const featuresUl= document.querySelector(".features-ul");
+const companyBtn = document.getElementById("company-btn");
+const companyUl = document.querySelector(".company-ul");
+
 
 menuBtn.onclick = () =>
   (navbar.style.display = navbar.style.display == "none" ? "block" : "none");
 closeBtn.onclick = () => (navbar.style.display = "none");
 
+featuresBtn.onclick = () =>
+  (featuresUl.style.display = featuresUl.style.display == "none" ? "block" : "none");
 
+companyBtn.onclick=()=>
+(companyUl.style.display = companyUl.style.display == "none" ? "block" : "none");
